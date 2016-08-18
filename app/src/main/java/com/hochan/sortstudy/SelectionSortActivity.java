393609e3babeb30769922a513c1610d6e3cceb49
zhendong_chen <zhendong_chen@kingdee.com>
2016-08-18 19:08:03 +0800

    选择排序

--- app/src/main/java/com/hochan/sortstudy/SelectionSortActivity.java
+++ app/src/main/java/com/hochan/sortstudy/SelectionSortActivity.java
@@ -1,16 +1,18 @@
 package com.hochan.sortstudy;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SelectionSortActivity extends BaseSortActiviy {
 
-	static void selection_sort(int[] unsorted){
-		for(int i = 0; i < unsorted.length; i++){
+	static void selection_sort(int[] unsorted) {
+		for (int i = 0; i < unsorted.length; i++) {
 			int min = i;
-			for(int j = i; i < unsorted.length; j++){
-				if(unsorted[min] > unsorted[j]){
+			for (int j = i; i < unsorted.length; j++) {
+				if (unsorted[min] > unsorted[j]) {
 					min = j;
 				}
 			}
@@ -27,8 +29,69 @@ public class SelectionSortActivity extends BaseSortActiviy {
 		textView.setText(R.string.des_selection_sort);
 	}
 
+	int mI = -1;
+	int mMinIndex;
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 	@Override
 	public void performSort(int step) {
+		mI++;
+		if(mI >= mUnsorted.length){
+			mI = 0;
+		}
+		mMinIndex = mI;
+		TextView tvI = mIntToTv.get(mUnsorted[mI]);
+		tvI.animate().translationY(mTranslationY).scaleX(0.6f).scaleY(0.6f).setDuration(500).withEndAction(new Runnable() {
+			@Override
+			public void run() {
+				performCompare(mI + 1);
+			}
+		}).start();
+	}
 
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	private void performCompare(final int j) {
+		if (j >= mUnsorted.length) {
+			TextView tvMin = mIntToTv.get(mUnsorted[mMinIndex]);
+			tvMin.animate().scaleX(1).scaleY(1).translationY(0).translationX((mI - mMinIndex) * mTranslationX + tvMin.getTranslationX()).setDuration(500).start();
+			TextView tvI = mIntToTv.get(mUnsorted[mI]);
+			tvI.animate().translationY(0).translationX(-(mI - mMinIndex) * mTranslationX + tvI.getTranslationX()).setDuration(500).withEndAction(new Runnable() {
+				@Override
+				public void run() {
+					int tmp = mUnsorted[mI];
+					mUnsorted[mI] = mUnsorted[mMinIndex];
+					mUnsorted[mMinIndex] = tmp;
+					showResult();
+				}
+			}).start();
+			return;
+		}
+		final TextView tvJ = mIntToTv.get(mUnsorted[j]);
+		tvJ.animate().translationY(mTranslationY).setDuration(500).withEndAction(new Runnable() {
+			@Override
+			public void run() {
+				if (mUnsorted[j] < mUnsorted[mMinIndex]) {
+					tvJ.animate().scaleX(0.6f).scaleY(0.6f).setDuration(500).start();
+					TextView tvTmp = mIntToTv.get(mUnsorted[mMinIndex]);
+					if (mMinIndex != mI) {
+						tvTmp.animate().translationY(0).setDuration(500).start();
+					}
+					tvTmp.animate().scaleY(1).scaleX(1).setDuration(500).withEndAction(new Runnable() {
+						@Override
+						public void run() {
+							mMinIndex = j;
+							performCompare(j + 1);
+						}
+					}).start();
+				} else {
+					tvJ.animate().translationY(0).setDuration(500).withEndAction(new Runnable() {
+						@Override
+						public void run() {
+							performCompare(j + 1);
+						}
+					}).start();
+				}
+			}
+		}).start();
 	}
 }
